77605cd46a2128032b0a74b42d87063f38fb0852
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-10-20 14:33:37 -0500

    Add pretty_duration_short

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # datime
 
 Python package with convenience functions for dates, times, and intervals
+
+## Usage
+
+### `pretty_duration_short`
+
+Use the function `pretty_duration_short` to convert a whole number of seconds to a string that indicates the number of
+hours, minutes, and seconds in the provided duration.
+
+```python
+>>> import datime
+>>> datime.pretty_duration_short(59)
+'59s'
+>>> datime.pretty_duration_short(256)
+'4m16s'
+>>> datime.pretty_duration_short(60 * 60)
+'1h0m0s'
+>>> datime.pretty_duration_short(4096)
+'1h8m16s'
+>>> datime.pretty_duration_short(2**17)
+'36h24m32s'
+```
--- src/datime/__init__.py
+++ src/datime/__init__.py
@@ -1,1 +1,20 @@
 __version__ = '2023.0'
+
+
+def pretty_duration_short(duration_seconds: int) -> str:
+    """Convert a duration in whole seconds to a string like '1h1m1s'. Durations less than 1 hour return '1m1s', and
+    durations less than 1 minute return '1s'.
+
+    >>> pretty_duration_short(12345)
+    '3h25m45s'
+    """
+    hours, seconds_remaining = divmod(duration_seconds, 3600)
+    minutes, seconds = divmod(seconds_remaining, 60)
+    parts = []
+    if hours > 0:
+        parts.append(f'{hours}h')
+        parts.append(f'{minutes}m')
+    elif minutes > 0:
+        parts.append(f'{minutes}m')
+    parts.append(f'{seconds}s')
+    return ''.join(parts)
